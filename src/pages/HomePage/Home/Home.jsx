6204961db9f0028c73ef2de788d41685febf9b73
questcoderull <questcoderull@gmail.com>
2025-12-02 13:06:06 +0600

    Added banner

--- src/pages/HomePage/Home/Home.jsx
+++ src/pages/HomePage/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
     <div>
-      <h1 className="text-2xl">Kahemul kabir</h1>
+      <Banner></Banner>
     </div>
   );
 };
